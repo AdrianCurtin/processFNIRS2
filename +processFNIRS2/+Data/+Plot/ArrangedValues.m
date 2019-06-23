b4cd5a0350bce83c1aa32eb606e6c6af25911751
Adrian Curtin <48138055+AdrianCurtin@users.noreply.github.com>
2019-06-22 22:46:16 -0400

    Update ArrangedValues.m
    Now has colorbar and supports titles

--- +processFNIRS2/+Data/+Plot/ArrangedValues.m
+++ +processFNIRS2/+Data/+Plot/ArrangedValues.m
@@ -1,13 +1,21 @@
-function [ figHandle ] = ArrangedValues(fNIR,data2plot,minVal,maxVal)
+function [ figHandle ] = ArrangedValues(fNIR,data2plot,minVal,maxVal,suptitleString,clrBarTitle)
 %processFNIRS2.Data.Plot.ArrangedValues
 %
 % Uses an imagemap to change the color of each cell based on data2plot
 %
-if(nargin<4)
+if(nargin<6)
+    clrBarTitle='';
+end
+
+if(nargin<5)
+   supTitleString=''; 
+end
+
+if(nargin<4||isempty(maxVal))
     maxVal=nanmax(data2plot);
 end
 
-if(nargin<3)
+if(nargin<3||isempty(minVal))
    minVal=nanmin(data2plot); 
 end
 
@@ -48,14 +56,54 @@ if(length(data2plot)~=probeInfo.NumOptodes)
 end
 
 
+h{length(data2plot)}= axes('Position',[0.90,0.05,0.045,0.9],'Box','on');
+
+numClrValues=1000;
+clrBarValues=minVal:(maxVal-minVal)/numClrValues:maxVal;
+clrBarValues(end)=maxVal;
+
+clrBarValues=clrBarValues(end:-1:1)';
+imagesc(clrBarValues,[minVal,maxVal]);
+
+caxis('manual')
+
+numTicks=11;
+
+ytickLocs=0:numClrValues/(numTicks-1):numClrValues;
+ytickLocs(1)=0;
+ytickLocs(end)=numClrValues;
+ytickVals=ytickLocs/numClrValues*(maxVal-minVal)+minVal;
+ytickLocs(1)=1;
+ytickVals(end)=maxVal;
+ytickLocs(end)=numClrValues-numClrValues/numTicks*0.05;
+ytickVals=ytickVals(end:-1:1);
+
+ytickLocs=round(ytickLocs);
+
+yticks(ytickLocs);
+clrBarTickLabels=cell(1,numTicks);
+for i=1:numTicks
+   clrBarTickLabels{i}=sprintf('%.2f', ytickVals(i));
+end
+yticklabels(clrBarTickLabels');
+set(gca,'YAxisLocation','right');
+set(gca,'xtick',[]);
+
+if(~isempty(clrBarTitle))
+    title(clrBarTitle);
+end
+
+
+
 
 h=cell(0);
 for(optIdx=1:length(data2plot))
     optNum=probeInfo.ChannelList(optIdx);
 
     optPos=probeInfo.OptLayout2D{optNum};
-    optPos([3,4])=optPos([3,4]).*[0.85,0.9];
+    optPos([3,4])=optPos([3,4]).*[0.8,0.9];
     optPos([1])=optPos([1])+0.01;
+    optPos([1])=optPos([1])/1.15;
     h{optIdx}= axes('Position',optPos,'Box','on');
     
 %     gh=gcf();
@@ -65,14 +113,19 @@ for(optIdx=1:length(data2plot))
 %     set(dcm_obj,'UpdateFcn', @myupdatefcn);
     
 
-    caxis('manual')
-    caxis([minVal,maxVal]);
+   
     
-    image(data2plot(optIdx));
+    imagesc(data2plot(optIdx));
+     caxis('manual')
+    caxis([minVal,maxVal]);
     axis off
     
     title(sprintf('Opt %i',optNum));
     
+end
+
+if(~isempty(suptitleString))
+    suptitle(suptitleString);
 end
 
 end
