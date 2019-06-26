1566fb1b767c17df7990874331549e70e094fac4
Adrian Curtin <48138055+AdrianCurtin@users.noreply.github.com>
2019-06-26 14:59:35 -0400

    Applied same edit to pf2.Process

--- +processFNIRS2/Process.m
+++ +processFNIRS2/Process.m
@@ -2,8 +2,9 @@ function varargout=Process(varargin)
 % This function is a wrapper for processFNIRS2's main function
 
 if(nargout>0)
-    [varargout(:)]=processFNIRS2(varargin{:});
+
+	varargout{1:nargout}=processFNIRS2(varargin{:});
 
 else
-    processFNIRS2(varargin{:});
+	processFNIRS2(varargin{:});
 end
