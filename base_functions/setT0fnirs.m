eb089b6b635bcb23d0f4fff4681d88cb9504c591
Adrian Curtin <adrian.b.curtin@gmail.com>
2019-08-25 15:49:31 -0400

    Update setT0fnirs.m
    removed legacy warning

--- base_functions/setT0fnirs.m
+++ base_functions/setT0fnirs.m
@@ -1,7 +1,7 @@
 function varargout=setT0fnirs(varargin)
 % This function is a wrapper function for processFNIRS2.Data.Split
 
-warning('Please replace with processFNIRS2.Data.SetT0()\n');
+%warning('Please replace with processFNIRS2.Data.SetT0()\n');
 
 if(nargout>0)
     varargout{1:nargout}=processFNIRS2.Data.SetT0(varargin{:});
