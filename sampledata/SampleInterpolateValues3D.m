3d0cadd2e63ee3962024859bb870d7abd872084c
Adrian Curtin <adrian.b.curtin@gmail.com>
2020-07-14 15:00:45 -0400

    Update SampleInterpolateValues3D.m
    Added animation sample

--- sampledata/SampleInterpolateValues3D.m
+++ sampledata/SampleInterpolateValues3D.m
@@ -29,7 +29,7 @@ processFNIRS2.Data.Plot.InterpolateValues3D(-7:8, 'minVal', [-2, 2], 'maxVal', 6
 %% Log scale
 pf2_base.loadDeviceCfg('Hitachi_ETG4000_3x5.cfg');
 data = [ones(1, 9) 1e-6 1e-5 1e-2 ones(1, 10)];
-processFNIRS2.Data.Plot.InterpolateValues3D(data, 'logScale', true, 'interpolateType', 'quadratic');
+processFNIRS2.Data.Plot.InterpolateValues3D(data, 'logScale', true, 'interpolateType', 'quadratic','minVal', 1e-8, 'maxVal', 0.9);
 
 %% Buffer distance
 subplot(1, 2, 1);
@@ -46,3 +46,19 @@ subplot(2, 1, 1);
 processFNIRS2.Data.Plot.InterpolateValues3D({1:16, 1:22}, {fnir1200, hitachi35}, 'initCamPosition', 'front', 'ChannelLabels', false, 'SDLabels', false);
 subplot(2, 1, 2);
 processFNIRS2.Data.Plot.InterpolateValues3D({1:16, 1:22}, {fnir1200, hitachi35}, 'initCamPosition', 'left', 'ChannelLabels', false, 'SDLabels', false);
+
+
+%% Animation test
+
+
+figure(10);
+ax=gca();
+
+fnir1200 = processFNIRS2.Import.SampleData.fNIR1200();
+
+optData=[(1:16)/16];
+
+for i=1:100
+    processFNIRS2.Data.Plot.InterpolateValues3D(optData.*sin(optData*i/20*pi), fnir1200,'ax',ax, 'initCamPosition', 'front', 'ChannelLabels', false, 'SDLabels', false,'minVal',0.4,'maxVal',1);
+    pause(0.1);
+end
