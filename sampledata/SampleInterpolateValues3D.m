f8b2246def92f15b2f7810a0f417d69e6a331d2b
Adrian Curtin <adrian.b.curtin@gmail.com>
2020-07-14 15:53:17 -0400

    restored camera toolbar
    Added cameratoolbar on first call
    
    Readded pause

--- sampledata/SampleInterpolateValues3D.m
+++ sampledata/SampleInterpolateValues3D.m
@@ -59,6 +59,7 @@ fnir1200 = processFNIRS2.Import.SampleData.fNIR1200();
 optData=[(1:16)/16];
 
 for i=1:100
+    
     processFNIRS2.Data.Plot.InterpolateValues3D(optData.*sin(optData*i/20*pi), fnir1200,'ax',ax, 'initCamPosition', 'front', 'ChannelLabels', false, 'SDLabels', false,'minVal',0.4,'maxVal',1);
-    %pause(0.05);
+    pause(0.0001);
 end
