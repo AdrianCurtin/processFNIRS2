083b5f24ada1c6716026da7f4e9febb652b1b931
Adrian Curtin <48138055+AdrianCurtin@users.noreply.github.com>
2022-07-26 21:52:45 -0400

    modify sett0 to support datetime parameters as an additional unit for time

--- +pf2/+Data/SetT0.m
+++ +pf2/+Data/SetT0.m
@@ -4,10 +4,28 @@ function outFNIR=SetT0(fnirStruct,t0time)
 %t0 is now 0;
 
 outFNIR=fnirStruct;
+
+if(isdatetime(t0time)&&isfield(outFNIR,'t0'))
+    % Ex: orig 15:01:25    new t0 is 15:00
+    % t0time = orig + 85s  
+    t0time=seconds(t0time-outFNIR.t0);
+    outFNIR.t0=t0time;
+elseif(isdatetime(t0time)&&sfield(outFNIR,'datetime'))
+    if(all(size(t0time))==all(size(outFNIR.datetime)))
+        outFNIR.t0=t0time;
+        outFNIR.time=seconds(outFNIR.datetime-outFNIR.t0);
+    end
+end
+
+
+
 if(isfield(outFNIR,'time'))
     outFNIR.time=outFNIR.time-t0time;
 end
 
+if(isfield(outFNIR,'datetime'))
+    outFNIR.datetime=outFNIR.t0+(duration(0,0,outFNIR.time));
+end
 
 if(isfield(outFNIR,'markers'))
    if(isfield(outFNIR.markers,'data'))
