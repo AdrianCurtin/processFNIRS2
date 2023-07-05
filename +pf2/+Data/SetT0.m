ca8eef257da983908e82dc2a5b18d4837c8c4f2d
Adrian Curtin <adrian.b.curtin@gmail.com>
2023-07-05 13:14:56 -0400

    Revalidate t0 calculation

--- +pf2/+Data/SetT0.m
+++ +pf2/+Data/SetT0.m
@@ -5,46 +5,56 @@ function outFNIR=SetT0(fnirStruct,t0time)
 
 outFNIR=fnirStruct;
 
-if(isdatetime(t0time)&&isfield(outFNIR,'t0'))
-    % Ex: orig 15:01:25    new t0 is 15:00
-    % t0time = orig + 85s  
-    tDiff=seconds(t0time-outFNIR.t0);
-    %outFNIR.t0=t0time;
 
-    t0time=tDiff;
-elseif(isdatetime(t0time)&&isfield(outFNIR,'datetime'))
-    % if datetime field is available, use the datetime to subtract
-    if(all(size(outFNIR.time)==size(outFNIR.datetime)))
-        %outFNIR.t0=outFNIR.datetime(1);
-        tdiff=outFNIR.time(1)-seconds(outFNIR.datetime(1)-outFNIR.t0);
-        t0time=tdiff;
+
+hasDatetime = isfield(outFNIR,'datetime');
+hasT0 = isfield(outFNIR,'t0');
+
+if(isduration(t0time))
+    tDiff=seconds(t0time);
+elseif(isdatetime(t0time))
+    if(hasT0)
+        outFNIR.t0=t0time;
+        % if new time is earlier, this should be negative
+        tDiff=seconds(t0time-fnirStruct.t0);
+    elseif(hasDatetime)
+        % if datetime field is available, use the datetime to subtract
+        if(all(size(outFNIR.time)==size(outFNIR.datetime)))
+            %outFNIR.t0=outFNIR.datetime(1);
+            tDiff=outFNIR.time(1)-seconds(outFNIR.datetime(1)-outFNIR.t0);
+            
+        else
+            error('All datetimes must be the same size as times');
+        end
     else
-        error('All datetimes must be the same size as times')
+        error('t0 cannot be set as a datetime if fnirs struct does not have datetime measures');
     end
+else
+    tDiff=t0time;
 end
 
 if(isfield(outFNIR,'time'))
-    outFNIR.time=outFNIR.time-t0time;
+    outFNIR.time=outFNIR.time-tDiff;
 end
 
 if(isfield(outFNIR,'t0')&&~isfield(outFNIR,'datetime'))
-    %datetime shouldn't be changing
+    %if we don't have datetime field, but do have t0, build datetime
     outFNIR.datetime=outFNIR.t0+(duration(0,0,outFNIR.time));
 elseif(~isfield(outFNIR,'t0')&&isfield(outFNIR,'datetime'))
-    %if we have a datetime, but not t0, make t0
+    %if we have datetime field, but don't have t0, build t0
     outFNIR.t0=outFNIR.datetime(1)-(duration(0,0,outFNIR.time(1)));
 end
 
 if(isfield(outFNIR,'t0'))
-    outFNIR.t0=outFNIR.t0+duration(0,0,t0time);
+    outFNIR.t0=fnirStruct.t0+duration(0,0,tDiff);
 end
 
 
 if(isfield(outFNIR,'markers'))
    if(isfield(outFNIR.markers,'data'))
-       outFNIR.markers.data(:,1)= outFNIR.markers.data(:,1)-t0time;
+       outFNIR.markers.data(:,1)= outFNIR.markers.data(:,1)-tDiff;
    elseif(~isempty(outFNIR.markers))
-      outFNIR.markers(:,1)= outFNIR.markers(:,1)-t0time;
+      outFNIR.markers(:,1)= outFNIR.markers(:,1)-tDiff;
    end
 end
 
@@ -56,14 +66,14 @@ if(pf2_base.isnestedfield(outFNIR,'Aux')) && ~isempty(outFNIR.Aux)
     auxFields=fields(outFNIR.Aux);
     for f=1:length(auxFields)
     	curFieldName=auxFields{f};
-        outFNIR.Aux.(curFieldName)(:,1) = outFNIR.Aux.(curFieldName)(:,1) -t0time; 
+        outFNIR.Aux.(curFieldName)(:,1) = outFNIR.Aux.(curFieldName)(:,1) -tDiff; 
     end
 end
 
 if(pf2_base.isnestedfield(outFNIR,'Aux.t'))
-    outFNIR.Aux.t(:,1)= outFNIR.Aux.t(:,1)-t0time;
+    outFNIR.Aux.t(:,1)= outFNIR.Aux.t(:,1)-tDiff;
 end
 
 if(pf2_base.isnestedfield(outFNIR,'Aux.time'))
-    outFNIR.Aux.time(:,1)= outFNIR.Aux.time(:,1)-t0time;
+    outFNIR.Aux.time(:,1)= outFNIR.Aux.time(:,1)-tDiff;
 end
